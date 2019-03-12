3717c13bbad47d4d68f4ebf2e174a09d32f3db16
Tali Escherˆ <t.escher@holidaypirates.com>
2019-03-12 12:09:34 +0100

    default config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="@/assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App - with a minor modification to test npm package"/>
   </div>
 </template>
@@ -8,14 +8,14 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import test from 'te-npm-package-test';
+// import test from 'te-npm-package-test';
 
 @Component({
   components: {
     HelloWorld,
   },
   mounted() {
-    console.log(test('mensaginha a ser transformada em caps'));
+    // console.log(test('mensaginha a ser transformada em caps'));
   },
 })
 export default class Home extends Vue {}
