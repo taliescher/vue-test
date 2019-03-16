d873233f698261713d92c4b707a75867c0a6cfbe
Tali Escher <taliescher@gmail.com>
2019-03-16 11:18:35 +0100

    Updates for GitHub Pages

---
+++ precache-manifest.9e3d5ac14230961156c0f70a3d557e4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-test/robots.txt"
+  },
+  {
+    "revision": "c16b454e15a55e746337",
+    "url": "/vue-test/js/chunk-vendors.a3ed09d0.js"
+  },
+  {
+    "revision": "a7194fa84a518fa0bc4d",
+    "url": "/vue-test/js/app.4b3b2ae1.js"
+  },
+  {
+    "revision": "7d705f9a1a77336ed921",
+    "url": "/vue-test/js/about.2b6705d6.js"
+  },
+  {
+    "revision": "cc3778a1f5b60077c69a4ee21e053c88",
+    "url": "/vue-test/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-test/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a7194fa84a518fa0bc4d",
+    "url": "/vue-test/css/app.183cfc78.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vue-test/precache-manifest.9e3d5ac14230961156c0f70a3d557e4d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-test"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
